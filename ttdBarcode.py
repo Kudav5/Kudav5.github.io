6839ce5fe8a9eb03d061bac0cd6b389bb6688e5a
Kudav5 <kdvalerian@gmail.com>
2025-01-02 13:32:29 +0700

    a

--- ttdBarcode.py
+++ ttdBarcode.py
@@ -6,7 +6,7 @@ from PIL import Image
 nm = ""
 gambar = "https://kudav5.github.io/"
 
-ean = barcode.get('code128', gambar, writer=ImageWriter()) # param2 = ganti teks(nm) / gambar
+ean = barcode.get('code128', gambar, writer=ImageWriter())''
 nama = agui.prompt('Masukkan nama barcode: ')
 ean.save(nama)
 ean = Image.open(nama + ".png")
